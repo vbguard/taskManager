a4348e208e58a90dd743e7ecef3efa36aa8f078d
StanislavKaisin <8050361@gmail.com>
2019-09-22 18:20:22 +0300

    fixed bugs

--- src/App.js
+++ src/App.js
@@ -5,10 +5,20 @@ import Login from './pages/Login/Login.jsx';
 import ProtectedComponent from './hoc/ProtectedComponent';
 import Task from './components/Task/Task.jsx';
 
+const task = {
+  taskNumber: 1, taskHeader:'Подготовка документации',
+  taskDescription:'Давно выяснено, что при оценке дизайна и композиции читаемый текст мешает сосредоточиться',
+  isLoop: true,
+  loopDates:[10,17,21], 
+  isComplete: true, 
+  onEdit: ()=> {}, 
+  onCompltete: ()=> {}
+}
+
 function App() {
   return (
     <Switch>
-      <Task/>
+      <Task task={task}/>
       <Redirect exact path="/" to="/dashboard" />
       <ProtectedComponent active={false} path="/login" component={Login} />
       <ProtectedComponent active={false} path="/dashboard" component={Dashboard} />
--- src/components/Task/Task.jsx
+++ src/components/Task/Task.jsx
@@ -2,76 +2,79 @@ import React, { Component } from 'react'
 import PropTypes from 'prop-types';
 import styles from './Task.module.css';
 
-import * as Icons from '../../assets/icons';
+import windowSize from 'react-window-size';
 import Icon from '../../components/Icon/Icon';
 
-const arrow = Icon(Icons.Arrow);
-
-console.log('Icons.Arrow=', Icons.Arrow);
-console.log('typeof Icons.Arrow=', typeof Icons.Arrow);
-
-console.log('arrow=', arrow);
-console.log('typeof arrow=', typeof arrow);
-
-console.log('Icon=', Icon);
-console.log('typeof Icon=', typeof Icon);
-
-export default class Task extends Component {
-
+class Task extends Component {
+    
     render() {
+        const {taskNumber, taskHeader, taskDescription, isLoop, loopDates, isComplete, onEdit, onCompltete}=this.props.task;
+        const windowWidth =this.props.windowWidth;
+        // const refactoringDates=loopDates.split(',');
+        console.log('loopDates=', loopDates);
+        // console.log(this.props);
         return (
             <>
+            {/* added fixed html to test */}
             <div className={styles.task}>
-                <div className={styles.taskHeader}>
+                <div className={isComplete ? styles.taskHeaderInactive : styles.taskHeader}>
                     <div className={styles.numberContainer}>
-                        <p className={styles.headerNumber}>1. </p>
+                        <p className={styles.headerNumber}>{taskNumber}. </p>
                     </div> 
                     <div className={styles.textContainer}>
-                        <p className={styles.headerText}>Подготовка документации</p>
+                        <p className={styles.headerText}>{taskHeader}</p>
                     </div> 
                 </div>
                 <div className={styles.taskBody}>
-                    <p>Давно выяснено, что при оценке дизайна и композиции читаемый текст мешает сосредоточиться</p>
+                    <p>{taskDescription}</p>
                 </div>
                 <div className={styles.taskControls}>
+                    {isLoop &&(
                     <div className={styles.taskControlsRepeat}>
                         <button type="button">
                             <Icon icon='Loop'/>
                         </button>
-                        <p>17,19,24</p>
+                        <p className={styles.taskControlsDates}>{loopDates}</p>
                     </div>
+                    )}
                     <div className={styles.taskControlsCompleteContainer}>
-                        <button type="button">
+                        <button className={styles.taskControlsEdit} type="button" onClick={onEdit}>
                             <Icon icon='Edit'/>
                         </button>
-                        <button type="button" className={styles.taskControlsDone}>
+                        {windowWidth>768? <p>Редактировать</p>:null}
+                        <button type="button" className={styles.taskControlsDone} onClick={onCompltete}>
                             <Icon icon='Done'/>
                         </button>
+                        {windowWidth>768? <p>Выполнить</p>:null}
                     </div>
                 </div>
             </div>
-            <div className={styles.task}>
-            <div className={styles.taskHeader}>
-                <div className={styles.numberContainer}>
-                        <p className={styles.headerNumber}>2. </p>
-                </div> 
-                <div className={styles.textContainer}>
-                        <p className={styles.headerText}>Разработка интерфейсов: разбор заданий</p>
-                </div>
-            </div>
-            <div className={styles.taskBody}>
-                    <p>Этот трактат по теории этики был очень популярен в эпоху Возрождения. Первая строка Lorem Ipsum, "Lorem ipsum dolor sit amet..", происходит от одной из строк в разделе 1.10.32</p>
-            </div>
-            <div className="taskControls">
-                
-                <p>taskControls</p>
-            </div>
-        </div>
-        </>
+            </>
         )
     }
 }
 
-Task.propTypes = {
-    prop: PropTypes
+Task.propTypes = {task: PropTypes.shape({
+    taskNumber: PropTypes.number.isRequired,
+    taskHeader: PropTypes.string.isRequired,
+    taskDescription: PropTypes.string.isRequired,
+    isLoop: PropTypes.bool.isRequired,
+    loopDates:PropTypes.arrayOf(PropTypes.number).isRequired,
+    isComplete: PropTypes.bool.isRequired,
+    onEdit: PropTypes.func.isRequired,
+    onCompltete: PropTypes.func.isRequired,})
+    
 }
+
+// Task.defaulProps = {
+//     taskNumber: null,
+//     taskHeader: '',
+//     taskDescription: '',
+//     isLoop: false,
+//     loopDates: null,
+//     isComplete: false,
+//     onEdit: () => {},
+//     onCompltete:  () => {},
+// }
+
+export default windowSize(Task);
