823eb6d5825f7207194b8b71f410cf4aede156f7
StanislavKaisin <8050361@gmail.com>
2019-09-23 18:56:20 +0300

    fixed some bugs

--- src/components/TaskList/TaskList.jsx
+++ src/components/TaskList/TaskList.jsx
@@ -1,18 +1,21 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
+import styles from './TaskList.module.css';
+
 import Task from '../../components/Task/Task.jsx';
+
 
 export default class TaskList extends Component {
     render() {
         const tasks = this.props.tasks;
         return (
-            <div>
+            <ul>
                 {tasks.map((task) => {
-                   return <Task task={task}/>
+                   return <li key={task.id}><Task task={task}/></li>
                 }
                 )}
-            </div>
+            </ul>
         )
     }
 }
