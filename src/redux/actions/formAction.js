f1093b97679d5aa3210ccacfbc44e428956e98ac
Svetlana <VSveta.83@gmail.com>
2019-09-23 18:32:47 +0300

    3

--- src/redux/actions/formAction.js
+++ src/redux/actions/formAction.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
 
+
 export const formTypes = {
   ADD_TASK_FORM_SUCCESS: 'ADD_TASK_FORM_SUCCESS',
   POST_TASK_LOADER: 'POST_TASK_LOADER',
@@ -27,11 +28,15 @@ export const postTaskError = error => ({
 });
 
 export const addTask = () => dispatch => {
-const data = JSON.parse( localStorage.getItem("persist:session"))
-console.log(data.token)
+  const data = JSON.parse(localStorage.getItem('persist:session'));
   dispatch(postTaskLoader(true));
   axios
-    .post('https://task-manager.goit.co.ua/api/task/create',{headers:{Authorization:localStorage.getItem("persist:session")}})
+    .post(
+      'https://task-manager.goit.co.ua/api/task/create',
+      {headers: {
+        Authorization: JSON.stringify (`Bearer ${data.token}`)
+      }}
+    )
     .then(response => {
       console.log(response);
       dispatch(postTaskLoader(false));
@@ -39,3 +44,7 @@ console.log(data.token)
     })
     .catch(error => dispatch(postTaskError(error)));
 };
+
+// {
+//   headers: { Authorization: JSON.stringify(data.token) }
+// }
