932bad44e163fc4a060fa9a7b3a7e7f57c5f3567
Liubov Murarova <murarova.lubov@gmail.com>
2019-09-23 20:23:54 +0300

    auth style fin

--- src/utils/requests.js
+++ src/utils/requests.js
@@ -1,22 +1,23 @@
-import axios from 'axios';
+import axios from "axios";
 // import { loginSuccess, loginError, authRequest } from '../redux/actions/authActions';
-import * as api from './entyPoints';
+import * as api from "./entyPoints";
 
-axios.defaults.headers.post['Content-Type'] = 'application/json';
-axios.defaults.headers.get['Content-Type'] = 'application/json';
-axios.defaults.headers.put['Content-Type'] = 'application/json';
+axios.defaults.headers.post["Content-Type"] = "application/json";
+axios.defaults.headers.get["Content-Type"] = "application/json";
+axios.defaults.headers.put["Content-Type"] = "application/json";
 
 const setToken = token => ({
-  headers: {
-    Authorization: `Bearer ${token}`
-  }
+	headers: {
+		Authorization: `Bearer ${token}`
+	}
 });
+
 export const requestUserLogin = credentials => {
-  const res = axios.post(api.url.loginUser(), credentials);
-  return res;
+	const res = axios.post(api.url.loginUser(), credentials);
+	return res;
 };
 
 export const fetchPosts = async credentials => {
-  const res = await axios.get(api.url.getTasks(), setToken(credentials));
-  return res;
+	const res = await axios.get(api.url.getTasks(), setToken(credentials));
+	return res;
 };
