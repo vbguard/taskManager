273a4130346e47be4c73c82d0689472ddb421c20
Liubov Murarova <murarova.lubov@gmail.com>
2019-09-24 15:12:00 +0300

    axios instance

--- src/redux/actions/authOperations.js
+++ src/redux/actions/authOperations.js
@@ -1,19 +1,20 @@
 import axios from "axios";
+import { requestUserLogin } from "../../utils/requests";
 import {
 	authRequest,
 	loginSuccess,
 	loginError,
 	logoutSuccess
 } from "./authActions";
-import { setAuthToken, clearAuthToken } from "../../config/axiousConfig";
+import { setAuthToken, clearAuthToken } from "../../utils/requests";
 import * as notify from "../../utils/notification";
 
 export const auth = credentials => dispatch => {
 	dispatch(authRequest());
 
-	axios
-		.post("https://task-manager.goit.co.ua/api/auth", credentials)
+	requestUserLogin(credentials)
 		.then(({ data }) => {
+			console.log(data.token);
 			setAuthToken(data.token);
 			dispatch(loginSuccess(data));
 		})
