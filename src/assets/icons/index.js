1acbc6c3ae1fd124a374a9b25fa15767e9d86677
Liubov Murarova <murarova.lubov@gmail.com>
2019-09-25 19:26:21 +0300

    calendar start

--- src/assets/icons/index.js
+++ src/assets/icons/index.js
@@ -1,15 +1,33 @@
-import { ReactComponent as Arrow } from './arrow.svg';
-import { ReactComponent as Exit } from './exit.svg';
-import { ReactComponent as Logout } from './logout.svg';
-import { ReactComponent as ArrowBack } from '../icons/arrow_back.svg';
-import { ReactComponent as Calendar } from '../icons/calendar_today.svg';
-import { ReactComponent as Clear } from '../icons/clear.svg';
-import { ReactComponent as Delete } from '../icons/delete.svg';
-import { ReactComponent as Done } from '../icons/done.svg';
-import { ReactComponent as Info } from '../icons/info_outline.svg';
-import { ReactComponent as ArrowRight } from '../icons/keyboard_arrow_right.svg';
-import { ReactComponent as Loop } from '../icons/loop.svg';
-import { ReactComponent as List } from '../icons/view_list.svg';
-import { ReactComponent as Edit } from '../icons/edit.svg';
+import { ReactComponent as Arrow } from "./arrow.svg";
+import { ReactComponent as Exit } from "./exit.svg";
+import { ReactComponent as Logout } from "./logout.svg";
+import { ReactComponent as ArrowBack } from "../icons/arrow_back.svg";
+import { ReactComponent as Calendar } from "../icons/calendar_today.svg";
+import { ReactComponent as Clear } from "../icons/clear.svg";
+import { ReactComponent as Delete } from "../icons/delete.svg";
+import { ReactComponent as Done } from "../icons/done.svg";
+import { ReactComponent as Info } from "../icons/info_outline.svg";
+import { ReactComponent as ArrowRight } from "../icons/keyboard_arrow_right.svg";
+import { ReactComponent as Loop } from "../icons/loop.svg";
+import { ReactComponent as List } from "../icons/view_list.svg";
+import { ReactComponent as Edit } from "../icons/edit.svg";
+import { ReactComponent as Back } from "../icons/back.svg";
+import { ReactComponent as Next } from "../icons/next.svg";
 
-export { Arrow, Edit, Exit, Logout, ArrowBack, Calendar, Clear, Delete, Done, Info, ArrowRight, Loop, List };
+export {
+	Arrow,
+	Edit,
+	Exit,
+	Logout,
+	ArrowBack,
+	Calendar,
+	Clear,
+	Delete,
+	Done,
+	Info,
+	ArrowRight,
+	Loop,
+	List,
+	Back,
+	Next
+};
--- src/components/Calendar/Calendar.js
+++ src/components/Calendar/Calendar.js
@@ -1,12 +1,46 @@
 import React, { Component } from "react";
 import styles from "./calendar.module.css";
+import { compareAsc, format } from "date-fns";
+import Icon from "../../components/Icon/Icon";
 
 class Calendar extends Component {
-	state = {};
+	state = {
+		currentMonth: new Date(),
+		selectedDate: new Date()
+	};
+
+	renderHeader() {
+		const dateFormat = "MMMM yyyy";
+		return (
+			<div className="header row flex-middle">
+				<div className="col col-start">
+					<div className={styles.icon} onClick={this.prevMonth}>
+						<Icon icon="Back" />
+					</div>
+				</div>
+				<div className="col col-center">
+					<span>{format(this.state.currentMonth, dateFormat)}</span>
+				</div>
+				<div className="col col-end" onClick={this.nextMonth}>
+					<div className={styles.icon}>
+						<Icon icon="Next" />
+					</div>
+				</div>
+			</div>
+		);
+	}
+	renderDays() {}
+	renderCells() {}
+
+	onDateClick = day => {};
+	nextMonth = () => {};
+	prevMonth = () => {};
+
 	render() {
 		return (
 			<div className={styles.calendarContainer}>
 				<h2>Calendar</h2>
+				{this.renderHeader()}
 			</div>
 		);
 	}
