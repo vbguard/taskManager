3a913b973435d4e0f5bc569106373ed046b0740c
Liubov Murarova <murarova.lubov@gmail.com>
2019-09-26 16:48:31 +0300

    infinite

--- src/components/Calendar/Calendar.js
+++ src/components/Calendar/Calendar.js
@@ -1,12 +1,17 @@
 import React, { Component } from 'react';
 import styles from '../../pages/CalendarPage/calendarPage.module.css';
+import InfiniteCalendar from 'react-infinite-calendar';
 
 class Calendar extends Component {
   state = {};
+
   render() {
+    const today = new Date();
+    const lastWeek = new Date(today.getFullYear(), today.getMonth(), today.getDate() - 7);
+
     return (
       <div className={styles.calendarContainer}>
-        <h2>Calendar</h2>
+        <InfiniteCalendar selected={today} minDate={lastWeek} />
       </div>
     );
   }
