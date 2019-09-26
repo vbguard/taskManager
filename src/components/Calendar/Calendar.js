bcf60187b070a70e835fac384f5c3039c5ccae76
Liubov Murarova <murarova.lubov@gmail.com>
2019-09-26 16:36:33 +0300

    calendar/styles

--- src/components/Calendar/Calendar.js
+++ src/components/Calendar/Calendar.js
@@ -1,15 +1,15 @@
-import React, { Component } from "react";
-import styles from "./calendar.module.css";
+import React, { Component } from 'react';
+import styles from '../../pages/CalendarPage/calendarPage.module.css';
 
 class Calendar extends Component {
-	state = {};
-	render() {
-		return (
-			<div className={styles.calendarContainer}>
-				<h2>Calendar</h2>
-			</div>
-		);
-	}
+  state = {};
+  render() {
+    return (
+      <div className={styles.calendarContainer}>
+        <h2>Calendar</h2>
+      </div>
+    );
+  }
 }
 
 export default Calendar;
