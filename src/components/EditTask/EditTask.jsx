19093a17dd893feb6c7fde5aee912504e1d578aa
Liubov Murarova <murarova.lubov@gmail.com>
2019-09-29 18:05:21 +0300

    merge

--- src/components/EditTask/EditTask.jsx
+++ src/components/EditTask/EditTask.jsx
@@ -92,55 +92,7 @@ class EditTask extends Component {
     return (
       <div className={style.bodybg}>
         <form onSubmit={this.handleSubmit} className={style.formBg}>
-<<<<<<< HEAD
-          <div className={style.contenctContainer}>
-            <input
-              name="title"
-              type="text"
-              value={title}
-              onChange={this.handleChange}
-              placeholder="#1 Введите название задачи"
-              className={style.title}
-            ></input>
-            {title.length > 50 && (
-              <span className={style.errorSpan}>Описание не должно быть больше 50-ти символов</span>
-            )}
-            <div className={style.dataPickerContainer} onClick={this.handleOpenDatePicker}>
-              <Icon icon="Calendar" className={style.formIcon} />
-              <p className={style.dataPickerTitle}>Выберете дату</p>
-              <Icon icon="ArrowRight" className={style.formIcon} />
-            </div>
-            {modal ? (
-              <Modal>
-                <DatePicker modal={modal} handleOpenDatePicker={this.handleOpenDatePicker} />
-              </Modal>
-            ) : (
-              ''
-            )}
-            <label htmlFor="description" className={style.labelDescription}>
-              Краткое описание:
-            </label>
-            <textarea
-              name="description"
-              wrap="virtual"
-              className={style.textArea}
-              onChange={this.handleChange}
-              value={description}
-              placeholder="Введите описание задачи"
-            ></textarea>
-            {description.length > 200 && <span>Описание не должно быть больше 200-ти символов</span>}
-            <button type="button" className={style.deleteBtn}>
-              <Icon icon="Delete" className={style.formIconDelete} onClick={confirmDelete} />
-            </button>
-            <button type="submit" className={style.saveBtn}>
-              Сохранить
-            </button>
-            <button type="reset" className={style.resetBtn} onClick={this.handleReset}>
-              Отмена
-            </button>
-          </div>
-=======
-                <input
+          <input
             name="title"
             type="text"
             value={title}
@@ -154,8 +106,10 @@ class EditTask extends Component {
             <p className={style.dataPickerTitle}>Выберете дату</p>
             <Icon icon="ArrowRight" className={style.formIcon} />
           </div>
-          {this.state.isToggleOn ? (
-            <DatePicker isToggleOn={isToggleOn} handleOpenDatePicker={this.handleOpenDatePicker} />
+          {modal ? (
+            <Modal>
+              <DatePicker modal={modal} handleOpenDatePicker={this.handleOpenDatePicker} />
+            </Modal>
           ) : (
             ''
           )}
@@ -170,20 +124,20 @@ class EditTask extends Component {
             value={description}
             placeholder="Введите описание задачи"
           ></textarea>
-          {description.length > 200 && <span className={style.errorSpan}>Описание не должно быть больше 200-ти символов</span>}
-         
+          {description.length > 200 && (
+            <span className={style.errorSpan}>Описание не должно быть больше 200-ти символов</span>
+          )}
           <button type="button" className={style.deleteBtn}>
             <Icon icon="Delete" className={style.formIconDelete} onClick={confirmDelete} />
           </button>
           <div className={style.battonContainer}>
-          <button type="submit" className={style.saveBtn}>
-            Сохранить
-          </button>
-          <button type="reset" className={style.resetBtn} onClick={this.handleReset}>
-            Отмена
-          </button>
-         </div>
->>>>>>> 926fcc70689144e48ddfb07b1be3011949e3f7d0
+            <button type="submit" className={style.saveBtn}>
+              Сохранить
+            </button>
+            <button type="reset" className={style.resetBtn} onClick={this.handleReset}>
+              Отмена
+            </button>
+          </div>
         </form>
       </div>
     );
