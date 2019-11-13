5fa1cdc776900493cd2825e951587bab72141860
Viktor Bulvarenko <silken.ua@gmail.com>
2019-11-13 23:24:26 +0200

    Updates

---
+++ precache-manifest.ecd8fda2824770402d4a128ec383d67a.js
@@ -0,0 +1,126 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f055284490e08ebb46e95d803f17c10",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d88effd21fbb42ee4dcc",
+    "url": "/static/css/2.90324bb6.chunk.css"
+  },
+  {
+    "revision": "93ae067ecd712b95bbd0",
+    "url": "/static/css/main.fe3a8f31.chunk.css"
+  },
+  {
+    "revision": "d88effd21fbb42ee4dcc",
+    "url": "/static/js/2.1e22a130.chunk.js"
+  },
+  {
+    "revision": "93ae067ecd712b95bbd0",
+    "url": "/static/js/main.56449862.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "f4ba25ecad3de799c38a0775177bd1cc",
+    "url": "/static/media/arrow.f4ba25ec.svg"
+  },
+  {
+    "revision": "ac9440512ba56b8f58e72384a30b0cb6",
+    "url": "/static/media/arrow_back.ac944051.svg"
+  },
+  {
+    "revision": "290174b6259ea557de7fde682836d3c6",
+    "url": "/static/media/back.290174b6.svg"
+  },
+  {
+    "revision": "0df2e63457c321bf3f6cb25fcd112c91",
+    "url": "/static/media/bg-header.0df2e634.jpg"
+  },
+  {
+    "revision": "4ef06c4717c3c1c747fff77144e872e9",
+    "url": "/static/media/calendar_today.4ef06c47.svg"
+  },
+  {
+    "revision": "777b93c6d5a7b86081144f8d0d3ca1d8",
+    "url": "/static/media/clear.777b93c6.svg"
+  },
+  {
+    "revision": "d36bd4a7a15f50d1a42ad58049254a48",
+    "url": "/static/media/delete.d36bd4a7.svg"
+  },
+  {
+    "revision": "e249914e9d3c8c92e0059c063c950353",
+    "url": "/static/media/done.e249914e.svg"
+  },
+  {
+    "revision": "45b30f6af163a69443f70b9e363a9a6e",
+    "url": "/static/media/edit.45b30f6a.svg"
+  },
+  {
+    "revision": "71ba7238156b843822dc93a1e200dbda",
+    "url": "/static/media/exit.71ba7238.svg"
+  },
+  {
+    "revision": "12592b3a22047e18cd35cb8902a82076",
+    "url": "/static/media/gotham-pro-black.12592b3a.woff"
+  },
+  {
+    "revision": "226d519086a3835e16c87f4ab1cd5641",
+    "url": "/static/media/gotham-pro-black.226d5190.woff2"
+  },
+  {
+    "revision": "37c37df55859d300a0bc06ebab70beda",
+    "url": "/static/media/gotham-pro-black.37c37df5.ttf"
+  },
+  {
+    "revision": "3f9c5a2b33fd28799ac8e55c093a4531",
+    "url": "/static/media/gotham-pro-bold.3f9c5a2b.otf"
+  },
+  {
+    "revision": "ad1867c1adc22645240b3fd33a205f9e",
+    "url": "/static/media/gotham-pro-bold.ad1867c1.woff2"
+  },
+  {
+    "revision": "bfece6d8c1ec4da6086bc2ace36d5ba9",
+    "url": "/static/media/gotham-pro-bold.bfece6d8.woff"
+  },
+  {
+    "revision": "792e5785377b31db37697448eba44281",
+    "url": "/static/media/gotham-pro-light.792e5785.ttf"
+  },
+  {
+    "revision": "964e1508f92be294b2d5be7e8ef64192",
+    "url": "/static/media/gotham-pro-light.964e1508.woff2"
+  },
+  {
+    "revision": "a28799775b388e696c2b160f08596c24",
+    "url": "/static/media/gotham-pro-light.a2879977.woff"
+  },
+  {
+    "revision": "48611b9c796cba3c3db2aabe2aae85e6",
+    "url": "/static/media/info_outline.48611b9c.svg"
+  },
+  {
+    "revision": "11ea5efbd1c9e8f273869a6d8487a079",
+    "url": "/static/media/keyboard_arrow_right.11ea5efb.svg"
+  },
+  {
+    "revision": "a0c11df4e57857bdd663a77dea4f5023",
+    "url": "/static/media/logout.a0c11df4.svg"
+  },
+  {
+    "revision": "d1920255c3f892c4ec96682b335915c2",
+    "url": "/static/media/loop.d1920255.svg"
+  },
+  {
+    "revision": "ff2b96719fb93f9698bfe4070dea4e27",
+    "url": "/static/media/next.ff2b9671.svg"
+  },
+  {
+    "revision": "a74737aeaa9a8ec5c62a92db5bb65737",
+    "url": "/static/media/view_list.a74737ae.svg"
+  }
+]);
